706cc00f04721e323f0264e1eff1b67c011173f9
Florian Jenett <fjenett@gmail.com>
2013-10-19 13:55:10 +0200

    little cleanup

--- app-base/routes.js
+++ app-base/routes.js
@@ -1,6 +1,3 @@
-// The routes for the application. This module returns a function.
-// `match` is match method of the Router
-
 module.exports = function(match) {
 
 	match( 'set/:setid', 'set#show' 
