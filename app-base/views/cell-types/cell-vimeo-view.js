f4c5bf035072e090fe6c47e12c23a1c7c821df77
Martin Leopold <m@martinleopold.com>
2013-11-01 15:15:59 +0100

    added 'loop' and 'autoplay' fields to vimeo cell
    resolves issues #30 and #25

--- app-base/views/cell-types/cell-vimeo-view.js
+++ app-base/views/cell-types/cell-vimeo-view.js
@@ -6,7 +6,9 @@ module.exports = CellIFrameView.extend({
 
 		CellIFrameView.prototype.initialize.apply(this,arguments);
 
-		this.model.set('iframe-src','http://player.vimeo.com/video/'+this.model.get('vimeo-id'));
+-		var loop = this.model.attributes['loop'] && /^true|1$/i.test(this.model.attributes['loop']);
+-		var autoplay = this.model.attributes['autoplay'] && /^true|1$/i.test(this.model.attributes['autoplay']);
+-		this.model.set( 'iframe-src','http://player.vimeo.com/video/' + this.model.get('vimeo-id') + '?autoplay=' + (autoplay ? 1 : 0) + '&loop=' + (loop ? 1 : 0) );
 
 		this.template = require('views/templates/cell-types/cell-iframe');
 	}
