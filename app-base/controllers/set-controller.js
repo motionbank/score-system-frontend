a6a4918fb4ea29b660d3b27c82517952e8298208
Martin Leopold <m@martinleopold.com>
2013-11-05 15:04:34 +0100

    Added error pages (controller + view). Catching non-existing sets, and all unmatched URLs.

--- app-base/controllers/set-controller.js
+++ app-base/controllers/set-controller.js
@@ -1,17 +1,27 @@
 var Controller = require('controllers/base/controller'),
     SetView    = require('views/set-view'),
-    SetModel   = require('models/set-model');
+    SetModel   = require('models/set-model'),
+    ErrorView    = require('views/error-view');
 
 module.exports = Controller.extend({
 
 	show : function ( opts ) {
 		
 		this.model = new SetModel();
-		this.model.fetch({id:opts.setid});
+
+		this.model.fetch({
+			id : opts.setid,
+
+			error : function(model, response, options) {
+				//Chaplin.helpers.redirectTo('error#show', {message: 'Error message'}); // use this to redirect to an error page (changes url)
+				new ErrorView( {message : 'Couldn\'t get the set you requested. (' + response.status + ')'} );
+			}
+		});
+
 		this.model.synced(function(){
 			setView.render();
 		});
-
+		
 		var setView = this.view = new SetView({
 			region: 'content',
 			model: this.model
---
+++ app-base/views/error-view.js
@@ -0,0 +1,24 @@
+var View = require('views/base/view');
+
+module.exports = View.extend({
+
+	autoRender: true,
+  	id: 'cover', // use #cover to get styling for free
+  	className: 'error container',
+  	region : 'content',
+  	containerMethod: 'html', // replace content instead of appending it by default
+
+	initialize : function (opts) {
+		if (!opts) opts = {};
+		this.opts = _.defaults(opts, {
+			'template' : 'default',
+			'message' : ''
+		});
+
+		this.template = require( 'views/templates/errors/' + opts.template );
+	},
+
+	getTemplateData : function () {
+		return this.opts;
+	}
+});
