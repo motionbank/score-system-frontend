d2f5519695882b1c9052d54bdde8d3b7b0e18cfc
Martin Leopold <m@martinleopold.com>
2013-11-14 01:13:57 +0100

    Revert "Revert c15bfbb..09cedc8"
    This reverts commit 49e0b6598004e8aff47389f5a71034a56cbf87fa.

--- app-base/views/cell-types/cell-iframe-view.js
+++ app-base/views/cell-types/cell-iframe-view.js
@@ -9,86 +9,47 @@ module.exports = CellDefaultView.extend({
 	// cell types have default values. can be overridden via fields
 	// these are the defaults use by most cell types
 	defaultViewAttributes : {
-		autoload : 1, // automatically open a cell on set load or scroll in?
-		sticky : 0, // close a cell on scroll out?
-		solo: 1 // close other cells on open?
+		autoload : 1, // open on scroll in?
+		sticky : 0, // don't close on scroll out?
+		solo: 0 // close other cells on open?
 	},
 
-	// events : {
-	// 	'click' : function () {
-	// 		this.renderContent();
-	// 	}
-	// },
-
-	// keep this in sync with cell-vimeo!
-	render : function () {
-
-		CellDefaultView.prototype.render.apply(this,arguments);
-
-		if ( this.model.attributes['autoload'] && 
-			/^(1|true)$/i.test(this.model.attributes['autoload']) ) {
-			this.renderContent();
+	events : {
+		'click' : function () {
+			// console.log("click"); 
+			if ( !this.isOpen ) {
+				this.open();
+			}
 		}
-
-		return this;
 	},
 
-	renderContent : function () {
-		// load iframe from data-src
-		$('iframe',this.$el).attr('src',$('iframe',this.$el).data('src'));
-
-		// connect to iframe with postmessenger
-		_(function(){
-
-			var view = this;
-			var iframe = $('iframe',this.$el);
-			
-			iframe.load(function() {
-				// show content, hide info
-				$('.element-hidden',view.$el).removeClass('element-hidden');
-				$('.info',view.$el).addClass('element-hidden');
-
-				view.$iframe = $(this);
-				// if iframe on another domain add that to the accept list
-				var src = view.model.get('iframe-src');
-				if ( /^http[s]?:\/\/.+/.test(src) ) {
-					var iframe_domain = (function(){
-						var p = src.split('/');
-						return p[0]+'//'+p[2];
-					})();
-					pm.accept(iframe_domain);
-				}
-
-				// connect it
-				pm.send(
-					'connect?',
-					{ listener_id: hasher.generate(20) }, 
-					this.contentWindow, 
-					iframe_domain
-				);
-			});
-			
-		}).bind(this).defer();
-
-		this.$el.css({
-			'background-image' : 'none'
+	initialize : function () {
+		CellDefaultView.prototype.initialize.apply(this,arguments);
+		// fill in missing view attributs with defaults
+		_.defaults(this.model.attributes, this.defaultViewAttributes);
+		
+		// listen for solo event. close if in same solo group
+		this.subscribeEvent('!solo', function(args) {
+			// console.log("solo ");
+			// console.log(args);
+			// skip if we sent that event
+			if ( args.origin == this ) { return; }
+			if ( args.group == this.model.get('solo') ) { this.close(); }
 		});
 	},
 
 	getTemplateData : function () {
 		var data = CellDefaultView.prototype.getTemplateData.apply(this,arguments);
+
 		if ( Handlebars.compile && data['iframe-src'] ) {
 			data['iframe-src'] = Handlebars.compile(data['iframe-src'])(config);
 		}
 		if ( data['iframe-src'] ) {
 			var spl = data['iframe-src'].split('?');
 			data['iframe-src'] = spl[0] + '?' + 'domain=http://' + config.host + '&' + (spl[1] || '');
-			// if ( false === /^http[s]:\/\/.+/.test(data['iframe-src']) ) {
-			// 	data['iframe-src'] = 'http://' + config.host + config.baseUrl + data['iframe-src'];
-			// }
 		}
 		
-		// collect attributes for <iframe>
+		// collect extra attributes for <iframe>
 		data.attr = {};
 		_.each(data.fields, function(element, index, list) {
 			if (element.name.substr(0,5) == 'attr-') {
@@ -99,65 +60,135 @@ module.exports = CellDefaultView.extend({
 		
 		return data;
 	},
-	
-	listen : {
-		// listen to changes of the active property
-		'change:focused model' : function(model, focused, options) {
-			if (focused) {
-				this.activate();
-				this.renderContent();
-			} else {
-				if (!model.isSticky()) this.deactivate();
-			}
+
+	render : function () {
+		CellDefaultView.prototype.render.apply(this,arguments);
+		
+		// when rendering is finished, meaning the <iframe> exists, connect postmessenger
+		_(this.connectPM).bind(this).defer();
+		
+		return this;
+	},
+
+	// render content
+	open : function () {
+		console.log("open " + this.cellInfo());
+
+		// show loading animation
+		this.showLoadingAnimation();
+
+		// load iframe from data-src
+		var $iframe = $('iframe',this.$el);
+		var that = this;
+		$iframe.one('load.open', function() { that.isOpen = true; }); // once loaded, set isOpen flag
+		$iframe.attr( 'src', $iframe.data('src') );
+		
+		$('.content',this.$el).removeClass('element-hidden');
+		$('.info',this.$el).addClass('element-hidden');
+		this.$el.css( 'background-image', 'none' );
+
+		// handle solo
+		// console.log("solo " + this.model.get('solo'));
+		if ( this.model.getFlag('solo') ) {
+			var soloGroup = this.model.get('solo');
+			// console.log('publish solo ' + soloGroup);
+			this.publishEvent('!solo', { group : soloGroup, origin : this });
 		}
 	},
 
+	// remove content
+	close : function () {
+		console.log("close " + this.cellInfo());
+		
+		var $iframe = $('iframe',this.$el);
+		$iframe.off('.open'); // don't set isOpen anymore, if pending
+		$iframe.attr( 'src', '');
+		
+		$('.content',this.$el).addClass('element-hidden');
+		$('.info',this.$el).removeClass('element-hidden');
+		this.$el.css( 'background-image', 'url('+this.model.getPosterImageURL()+')' );
+
+		this.isOpen = false;
+	},
+
+	// called when scrolled into view 
 	activate : function () {
-		if ( !this.active ) {
-			// console.log("activate iframe: " + this.cid);
-
-			this.active = true; // needs to be before render(). why? don't know...
-
-			if ( !this.model.isSticky() ) {
-				this.render();
-			} else {
-				if (!this.rendered) {
-					this.render();
-					var iframe = $('iframe', this.$el)[0];
-					var that = this;
-					$(iframe).one('load', function(){
-						pm.send(that.active ? 'activate!' : 'deactivate!', {}, this.contentWindow );
-						that.rendered = true;
-					});
-				} else {
-					var iframe = $('iframe', this.$el)[0];
-					pm.send( 'activate!', {}, iframe.contentWindow );
-			}}
-
+		// console.log( "activate " + this.model.get('type') + this.model.get('connection_id') );
+		if ( this.model.getFlag('autoload') && !this.isOpen ) {
+			this.open();
+		}
+		
+		if ( this.model.getFlag('sticky') && this.isOpen ) {
+			// console.log("sending activate");
+			this.sendPM('activate!');
 		}
 	},
 
+	// called when scrolled out of view
 	deactivate : function () {
-		if ( this.active ) {
-			if (!this.model.isSticky()) {
-				// console.log("deactivate iframe: " + this.cid);
-				//this.$el.empty();
-				// remove content and show info again
-				$('.content', this.$el).remove();
-				$('.info', this.$el).removeClass('element-hidden');
+		// console.log( "deactivate " + this.model.get('type') + this.model.get('connection_id') );
+		if ( !this.model.getFlag('sticky') ) {
+			this.close();
+		}
 
-				this.$el.css({
-					'background-image': 'url('+this.model.getPosterImageURL()+')'
-				});
+		if ( this.model.getFlag('sticky') && this.isOpen ) {
+			// console.log("sending deactivate");
+			this.sendPM('deactivate!');
+		}
+	},
 
-				this.rendered = false;
-			} else {
-				var iframe = $('iframe', this.$el)[0];
-				if (iframe)  {
-					pm.send('deactivate!', {}, iframe.contentWindow );
-				}
+	showLoadingAnimation : function () {
+		$('.loading', this.$el).show(0);
+		var that = this;
+		$('iframe', this.$el).one('load', function() {
+			$('.loading', that.$el).hide(0);
+		});
+	},
+
+	// connect postmessenger to iframe
+	// remains on the iframe and reconnects on load, so only call this once
+	connectPM : function () {
+		// console.log("connectPM " + this.cellInfo());
+		
+		var that = this;
+		$('iframe', this.$el).on('load', function() {
+			// load event is also fired on close, when src is set to "". skip this.
+			if (!this.src) {
+				// console.log("src not valid: " + this.src);
+				return;
 			}
-			this.active = false;
+
+			// if iframe on another domain add that to the accept list
+			var src = that.model.get('iframe-src');
+			if ( /^http[s]?:\/\/.+/.test(src) ) {
+				var iframe_domain = (function(){
+					var p = src.split('/');
+					return p[0]+'//'+p[2];
+				})();
+				pm.accept(iframe_domain);
+			}
+			// connect it
+			pm.send(
+				'connect?',
+				{ listener_id: hasher.generate(20) }, 
+				this.contentWindow,
+				iframe_domain
+			);
+		});
+	},
+	
+	sendPM : function (msg, opts) {
+		var iframe = $('iframe', this.$el)[0];
+		if (opts == undefined) { opts = {}; }
+
+		if (this.isOpen) {
+			// console.log("sending " + msg);
+			pm.send(msg, opts, iframe.contentWindow );
+		} else { // wait till iframe is loaded to send message
+			$(iframe).one('load', function() {
+				// console.log("late sending " + msg);
+				pm.send(msg, opts, this.contentWindow );
+			});
 		}
 	}
 });
--- app-base/views/set-view.js
+++ app-base/views/set-view.js
@@ -1,3 +1,4 @@
+/* set-view.js */	
 var View 	 = require('views/base/view');
 
 module.exports = View.extend({
@@ -7,7 +8,7 @@ module.exports = View.extend({
 
 		this.subscribeEvent('set:scrolled',function(val){
 			this.layoutAttributes.position = val;
-			this.updateVisibleCells();
+			this.updateCellVisibility();
 		});
 
 		this.subscribeEvent('window:resized',function(){
@@ -24,41 +25,42 @@ module.exports = View.extend({
 	template: require('views/templates/set'),
 
 	layoutAttributes : {
-		position : 0.0,
-		visible_x : 0,
-		visible_y : 0,
-		width : -1,
-		height : -1,
-		cell_width : 0,
-		cell_height : 0
+		position : 0.0, // scroll position in set [0,1]
+		visible_x : 0, // #cells that fit in one screen horizontally
+		visible_y : 0, // #cells that fit in one screen vertically
+		width : -1, // set width in px
+		height : -1, // set height in px
+		cell_width : 0, // cell width in px
+		cell_height : 0 // cell height in px
 	},
 
 	render : function () {
-
+		// console.log("rendering set");
 		View.prototype.render.apply(this,arguments);
 
 		this.layoutAttributes.position = 0.0;
 		
 		this.model.collectionView.render();
+
 		// call updateCellGrid() after render finished (for it to have width, height avail)
 		_(function(){
 			this.updateCellGrid();
-			this.updateVisibleCells();
+			this.establishCellGrid();
+			this.updateCellVisibility();
 		}).bind(this).defer();
 
 		return this;
 	},
 
-	updateLayoutSize : function () {
-		this.layoutAttributes.width  = this.$el.width();
-		this.layoutAttributes.height = this.$el.height();
-	},
-
+	// re-calculate layout attributes: set dimensions, grid dimensions (on resize)
 	updateCellGrid : function () {
+		// console.log("updateCellGrid");
 
 		var layAttrs = this.layoutAttributes;
 
-		this.updateLayoutSize();
+		// update layout size
+		layAttrs.width  = this.$el.width();
+		layAttrs.height = this.$el.height();
 		
 		var cell_height = parseInt( Math.floor( layAttrs.height / this.model.get('grid_rows') ) );
 		var cell_width  = (this.model.get('cell_width') / this.model.get('cell_height')) * cell_height;
@@ -78,68 +80,66 @@ module.exports = View.extend({
 		layAttrs.cell_width  = cell_width;
 		layAttrs.cell_height = cell_height;
 
-		//console.log( this.layoutAttributes );
+		// add media-query style classes to cells
+		_.each( this.model.collectionView.getItemViews(), function( cellView ) {
+		var cellDim = cellView.model.get('extra');
+			cellView.$el.attr( 'class', cellView.$el.attr('class').replace(/ cell-(width|height)-[0-9]+/ig,'') );
+			// console.log('cell-width-'+ (parseInt((layAttrs.cell_width*cellDim.width) /50)*50));
+			cellView.$el.addClass( 'cell-width-'+ (parseInt((layAttrs.cell_width*cellDim.width) /50)*50) );
+			cellView.$el.addClass( 'cell-height-'+(parseInt((layAttrs.cell_height*cellDim.height)/50)*50) );
+		});
 	},
 
-	updateVisibleCells : function () {
+	// establish css cell positions and dimensions (once)
+	establishCellGrid : function () {
+		// console.log("establishCellGrid");
 
 		var gridWidth = this.model.get('grid_cols');
 		var xFrom = Math.round( this.layoutAttributes.position * (gridWidth - this.layoutAttributes.visible_x) );
 
-		var cw = 100.0/gridWidth;
-		var ch = 100.0/this.layoutAttributes.visible_y;
+		var cw = 100.0/gridWidth;  // cell width (% of set)
+		var ch = 100.0/this.layoutAttributes.visible_y; // cell height (% of set)
 
-		var absCw = (1.0*this.layoutAttributes.width) /this.layoutAttributes.visible_x,
-			absCh = (1.0*this.layoutAttributes.height)/this.layoutAttributes.visible_y;
-
-		// TODO: clean this mess
-		_.each( this.model.collectionView.getItemViews(), 
-				(function(layAttrs){ return function( cellView, cid ) {
-
+		_.each( this.model.collectionView.getItemViews(), function( cellView ) {
 			var cellDim = cellView.model.get('extra');
 
-			cellView.$el.removeClass('lastcol').removeClass('lastrow');
+			var left  = cw * cellDim.x;
+			var width = cw * cellDim.width;
 
+			cellView.$el.css({
+				left: 	left+'%',
+				top: 	(ch*cellDim.y)+'%',
+				width: 	width+'%',
+				height: (ch*cellDim.height)+'%'
+			});
+		});
+	},
+
+	// send activation / deactivation to cells (on scroll)
+	updateCellVisibility : function () {
+		// console.log("updateVisibleCells");
+
+		var gridWidth = this.model.get('grid_cols');
+		var xFrom = Math.round( this.layoutAttributes.position * (gridWidth - this.layoutAttributes.visible_x) );
+		var layAttrs = this.layoutAttributes;
+
+		_.each( this.model.collectionView.getItemViews(), function( cellView ) {
+			var cellDim = cellView.model.get('extra');
 			// ... not too far left or right
 			if ( !( (cellDim.x + (cellDim.width-1)) < xFrom - 1 ||
 					 cellDim.x > xFrom + layAttrs.visible_x + 1 ) 
 				) {
-				
-				var left  = cw * cellDim.x;
-				var width = cw * cellDim.width;
-
-				// show, set position and size in % to make responsive
-				
-				cellView.activate();
-
-				cellView.$el.css({
-					left: 	left+'%',
-					top: 	(ch*cellDim.y)+'%',
-					width: 	width+'%',
-					height: (ch*cellDim.height)+'%'
-				});
-
-				// add media-query style classes to cells
-				cellView.$el.attr( 'class', cellView.$el.attr('class').replace(/cell-(width|height)-[0-9]+/ig,'') );
-				cellView.$el.addClass( 'cell-width-'+ (parseInt((absCw*cellDim.width) /50)*50) ).
-					   addClass( 'cell-height-'+(parseInt((absCh*cellDim.height)/50)*50) );
-
-				if ( (cellDim.x-xFrom) + cellDim.width >= layAttrs.visible_x ) {
-					cellView.$el.addClass('lastcol');
-				}
-				if ( cellDim.y+cellDim.height >= layAttrs.visible_y ) {
-					cellView.$el.addClass('lastrow');
+				if ( !cellView.isVisible ) { 
+					cellView.activate();
+					cellView.isVisible = true;
 				}
 			} else {
-				cellView.deactivate();
+				if ( cellView.isVisible ) {
+					cellView.deactivate();
+					cellView.isVisible = false;
+				}
 			}
-
-			// if ( showCellInfo ) {
-			// 	cellView.$el.addClass('with-info');
-			// } else {
-			// 	cellView.$el.removeClass('with-info');
-			// }
-		}})(this.layoutAttributes));
+		});
 	}
   
 });
