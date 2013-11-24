c430c6c3b207782e899c0865754fae4d6e6f4641
Martin Leopold <m@martinleopold.com>
2013-11-24 18:01:04 +0100

    Made whole set-link cell clickable
    resolves #51

--- app-base/views/cell-types/cell-set_link-view.js
+++ app-base/views/cell-types/cell-set_link-view.js
@@ -2,6 +2,21 @@ var CellDefaultView = require('views/cell-view');
 
 module.exports = CellDefaultView.extend({
 
+	events : {
+		// follow the link when clicking anywhere in the cell
+		'click' : function(e) {
+			e.preventDefault();
+			var href = this.$el.find('a').attr('href');
+			window.location = href;
+			return false;
+		}
+	},
+
+	initialize : function() {
+		CellDefaultView.prototype.initialize.apply(this,arguments);
+		this.$el.css('cursor', 'pointer'); // add pointer cursor to the whole cell
+	},
+
 	getTemplateData : function () {
 		var data = CellDefaultView.prototype.getTemplateData.apply(this,arguments);
 		data['link'] = '#/set/'+data['set-id'];
