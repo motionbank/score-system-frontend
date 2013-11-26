20dc2cf779745b8af5d50557a95fef1465b13665
Martin Leopold <m@martinleopold.com>
2013-11-26 15:38:10 +0100

    Added cell-id field to set_link cell
    Resolves #47

--- app-base/views/cell-types/cell-set_link-view.js
+++ app-base/views/cell-types/cell-set_link-view.js
@@ -1,4 +1,5 @@
-var CellDefaultView = require('views/cell-view');
+var CellDefaultView = require('views/cell-view'),
+	SetController = require('controllers/set-controller');
 
 module.exports = CellDefaultView.extend({
 
@@ -7,6 +8,13 @@ module.exports = CellDefaultView.extend({
 		'click' : function(e) {
 			e.preventDefault();
 			var href = this.$el.find('a').attr('href');
+
+			// only need to do this, if path is the same (to force jump to cell)
+			// console.log("INIT SC");
+			// var sc = new SetController();
+			// sc.show({setid: 'all-cell-types', cellid: 'b0c35f716d6d6d8d0daa0100'});
+			// console.log("LOC");
+
 			window.location = href;
 			return false;
 		}
@@ -19,7 +27,19 @@ module.exports = CellDefaultView.extend({
 
 	getTemplateData : function () {
 		var data = CellDefaultView.prototype.getTemplateData.apply(this,arguments);
-		data['link'] = '#/set/'+data['set-id'];
+		
+		// current set id/path
+		var current_set_id = this.model.collection.set_id, current_set_path = this.model.collection.set_path;
+
+		// fill set id if not given
+		if ( !data['set-id'] || data['set-id'] == '0' ) {
+			data['set-id'] = current_set_path || current_set_id; // use id if path is empty
+		}
+		// basic link
+ 		data['link'] = '#/set/' + data['set-id'];
+ 		// add cell id to link if given
+ 		if ( data['cell-id'] ) data['link'] += '/' + data['cell-id'];
+
 		data['title'] = data['title'];
 		return data;
 	}
